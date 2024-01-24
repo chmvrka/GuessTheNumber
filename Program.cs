08f20548d953c1287b7a4faed0c2e4d17926f740
chmvrka <juliaxmajewska2@gmail.com>
2024-01-24 16:54:38 +0100

    Make a game plan

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿Console.WriteLine("Let's play Guess The Number!");
 
+// 1. Ask the player for a range of numbers
+// 2. Random number generator
+// 3. Ask player to provide number
+// 4. Tell player which attempt it is and if the number is correct/to high/to low
+// 5. Sum Up if the player guessed
+// 6. Ask if the player wants to play again
+
 Console.ReadKey();
