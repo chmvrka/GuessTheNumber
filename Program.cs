6642818f9df1c164271c4bbcab66244fe1d5a5c4
chmvrka <juliaxmajewska2@gmail.com>
2024-01-24 17:28:53 +0100

    Ask the player for a range of numbers

--- Program.cs
+++ Program.cs
@@ -1,8 +1,19 @@
-﻿Console.WriteLine("Let's play Guess The Number!");
+﻿using System.ComponentModel;
+
+Console.WriteLine("Let's play Guess The Number!");
 
 // 1. Ask the player for a range of numbers
+Console.WriteLine("Provide a range of numbers between which you want to guess");
+
+Console.Write("Minimum: ");
+int.TryParse(Console.ReadLine(), out int minRange);
+
+Console.Write("Maximum: ");
+int.TryParse(Console.ReadLine(), out int maxRange);
+
 // 2. Random number generator
 // 3. Ask player to provide number
+Console.WriteLine ($"Guess a number between {minRange} and {maxRange}:");
 // 4. Tell player which attempt it is and if the number is correct/to high/to low
 // 5. Sum Up if the player guessed
 // 6. Ask if the player wants to play again
